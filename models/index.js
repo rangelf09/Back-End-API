351e52bc1352b031fba56d748f5d5a940dff93c2
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-11-08 17:09:53 -0600

    added the products belong to into index.js models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'categorey_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'product_tag',
+    unique: false,
+  },
+  as: "product_descrip"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany (Product, {
+  through: {
+    model: 'product_tag',
+    unique: false
+  },
+  as: 'product_type'
+});
 
 module.exports = {
   Product,
